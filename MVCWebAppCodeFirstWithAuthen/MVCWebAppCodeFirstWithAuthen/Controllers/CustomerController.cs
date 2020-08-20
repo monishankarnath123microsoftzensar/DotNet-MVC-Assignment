ffe6923ef297a76965ea0c4ea948e746b26106c5
monishankar nath <monishankar.nath@zensar.com>
2020-08-20 22:15:55 +0530

    Upload assignment of 20-08-2020

---
+++ MVCWebAppCodeFirstWithAuthen/MVCWebAppCodeFirstWithAuthen/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWebAppCodeFirstWithAuthen.Models;
+
+namespace MVCWebAppCodeFirstWithAuthen.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+        // GET: Customer
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var customer = _context.Customers.Include(c=>c.MembershipType).ToList();
+            return View(customer);
+        }
+        public ActionResult Details(int id)
+        {
+            var singleCustomer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (singleCustomer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(singleCustomer);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+    }
+}
---
+++ MVCWebAppCodeFirstWithAuthen/MVCWebAppCodeFirstWithAuthen/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebAppCodeFirstWithAuthen.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public string Gener { get; set; }
+    }
+}
